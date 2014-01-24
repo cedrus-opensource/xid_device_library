2b70acf1b81027e8118297704461b9554eea5cca
Eugene Matsak <matsak@cedrus.com>
2014-01-24 12:36:31 -0800

    A minor update to the rough draft build scripts.

---
+++ xid_device_driver/build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+export PATH="/Library/Frameworks/Python.framework/Versions/2.6/bin:$PATH"
+export PYTHONPATH="$PYTHONPATH:../SCons_build/"
+export PYTHONPATH="$PYTHONPATH:$SCONS_DIR"
+
+/usr/local/bin/scons  "$@"
