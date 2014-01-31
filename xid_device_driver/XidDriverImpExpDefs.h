8d586f7c781fb4b6c2f91c9c6a762fd03298850d
Eugene Matsak <matsak@cedrus.com>
2014-01-31 15:31:35 -0800

    Further refinements for the build scripts.

---
+++ xid_device_driver/XidDriverImpExpDefs.h
@@ -0,0 +1,34 @@
+
+
+
+#ifndef  CEDRUS_XID_DRIVER_IMP_EXP_DLL_DEF
+#define  CEDRUS_XID_DRIVER_IMP_EXP_DLL_DEF
+
+
+#include <boost/preprocessor/control/if.hpp>
+
+#ifdef CEDRUS_XID_MAKEDLL
+#    undef CEDRUS_XID_MAKEDLL
+
+#    define CEDRUS_XID_MAKEDLL    1
+#else
+#    define CEDRUS_XID_MAKEDLL    0
+#endif
+
+
+#ifdef __APPLE__
+#    define CEDEXP __attribute__ ((visibility("default")))
+#    define CEDIMP __attribute__ ((visibility("default")))
+#elif defined(_WIN32)
+
+#endif
+
+
+
+#define CEDRUS_XIDDRIVER_IMPORTEXPORT   BOOST_PP_IIF( CEDRUS_XID_MAKEDLL, CEDEXP, CEDIMP )
+
+
+
+#endif  // CEDRUS_XID_DRIVER_IMP_EXP_DLL_DEF
+
+
