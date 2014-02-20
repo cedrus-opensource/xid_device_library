e1353ba5ab0e68171be7b1e4809b9e211b25f4a6
Kelly Heller <kkheller@cedrus.com>
2014-02-20 14:39:43 -0800

    moving slowly towards a correctly-working build using scons_helpers

--- SCons_build/build.sh
+++ SCons_build/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export PATH="/Library/Frameworks/Python.framework/Versions/2.6/bin:$PATH"
-#export PYTHONPATH="$PYTHONPATH:../SCons_build/"
+export PYTHONPATH="$PYTHONPATH:../scons_helpers/scons/"
 export PYTHONPATH="$PYTHONPATH:$SCONS_DIR"
 
 /usr/local/bin/scons  "$@"
