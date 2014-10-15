4e380d63afdcc1df17ac8e7e69fc1aa1a994eb37
Eugene Matsak <matsak@cedrus.com>
2014-10-15 12:35:16 -0700

    Fixing a warning

--- xid_device_driver/interface_xid_con.h
+++ xid_device_driver/interface_xid_con.h
@@ -6,9 +6,6 @@ namespace cedrus
     class interface_xid_con
     {
     public:
-        virtual ~interface_xid_con()
-        {}
-
         virtual bool read( unsigned char *in_buffer, int bytes_to_read, int *bytes_read) = 0;
 
     };
