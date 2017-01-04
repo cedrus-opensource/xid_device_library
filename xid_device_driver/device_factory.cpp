d073f920a26a002b264ddd543add57eea9e44271
Eugene Matsak <matsak@cedrus.com>
2017-01-04 14:28:53 -0800

    Adding an XID2 version of the StimTracker class, as well as logic to create instances of it when necessary.

--- xid_device_driver/device_factory.cpp
+++ xid_device_driver/device_factory.cpp
@@ -4,35 +4,38 @@
 #include "xid_device_t.h"
 #include "xid2_device.h"
 #include "stim_tracker_t.h"
+#include "StimTracker2.h"
 #include "xid_device_config_t.h"
 
 #include <boost/foreach.hpp>
 
-boost::shared_ptr<cedrus::base_device_t>
-cedrus::create_device
+boost::shared_ptr<cedrus::base_device_t> cedrus::create_device
 (
- const int product_id, // d2_value
- const int model_id,   // d3 value
- const int major_firmware_version, // d4 value
- const std::vector< boost::shared_ptr<cedrus::xid_device_config_t> > &config_candidates,
- boost::shared_ptr<xid_con_t> xid_con
+    const int product_id, // d2_value
+    const int model_id,   // d3 value
+    const int major_firmware_version, // d4 value
+    const std::vector< boost::shared_ptr<cedrus::xid_device_config_t> > &config_candidates,
+    boost::shared_ptr<xid_con_t> xid_con
 )
 {
     boost::shared_ptr<cedrus::base_device_t> result;
 
-    // for the given set of devconfigs, can we identify the product/model as "one of our own" ?
+    // match the product/model id and firmware version to a devconfig
     BOOST_FOREACH(boost::shared_ptr<cedrus::xid_device_config_t> const config, config_candidates)
     {
-        if ( config->does_config_match_device(product_id, model_id, major_firmware_version) )
+        if (config->does_config_match_device(product_id, model_id, major_firmware_version))
         {
             // StimTracker or a c-pod
-            if( product_id == 'S' || static_cast<char>(product_id) == '4' )
+            if (product_id == 'S' || static_cast<char>(product_id) == '4')
             {
-                result.reset(new stim_tracker_t(xid_con, config));
+                if (major_firmware_version == 1)
+                    result.reset(new stim_tracker_t(xid_con, config));
+                else
+                    result.reset(new StimTracker2(xid_con, config));
             }
             else
             {
-                if ( major_firmware_version == 1 )
+                if (major_firmware_version == 1)
                 {
                     result.reset(new xid_device_t(xid_con, config));
                 }
@@ -42,15 +45,9 @@ cedrus::create_device
                 }
             }
 
-            /*
-              Important: each unique product should only ever match ONE devconfig file!
-                 So... as soon as we found a match, we can stop looping.
-             */
             break;
         }
     }
 
     return result;
 }
-
-
